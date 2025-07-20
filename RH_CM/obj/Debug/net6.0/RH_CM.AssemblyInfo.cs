08a19883f3efb76f8671ad999628471b46558ddc
Webmindzz <webmindzz@outlook.com>
2025-07-20 11:35:21 -0600

    Changes to CatalogoControllers

--- RH_CM/obj/Debug/net6.0/RH_CM.AssemblyInfo.cs
+++ RH_CM/obj/Debug/net6.0/RH_CM.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RH_CM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+626e909e6510696be75e8098716f0d417b222263")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7769a9d6791ac8857927a7497b34dd97a14ff42c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RH_CM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RH_CM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
